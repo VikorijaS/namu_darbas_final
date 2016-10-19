9d90f56f42ebf2b33ea37e02cf779b3c0abfd9dc
ViktorijaS <v.cerniavskyte@gmail.com>
2016-10-19 21:07:15 +0300

    intanceof

--- 5 paskaita/src/vcs/Main.java
+++ 5 paskaita/src/vcs/Main.java
@@ -1,6 +1,7 @@
 package vcs;
 
 import vcs.Uzduotis.User;
+import vcs.paketas.PaketoKlase;
 import vcs.users.Linas;
 import vcs.users.Viktorija;
 
@@ -8,12 +9,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Linas linas = new Linas ("raudona");
-
-        Viktorija viktorija = new Viktorija("moto");
-
-
+        User linas = new Linas ("raudona");
+        User viktorija = new Viktorija("moto");
         User useris = new User("vardauskas","pavardauskas");
+        User pk = new Viktorija("tekstas");
+
+        if (viktorija instanceof Viktorija) {
+
+            VcsUtils.println("Viktorija valio");
+            Viktorija tikraiVik = (Viktorija)viktorija;
+            VcsUtils.println(tikraiVik.getHobis());
+        }
+
+        if (pk instanceof User){
+            VcsUtils.println("Paketas valio");
+        }
 
         VcsUtils.println((useris).toString());
 
