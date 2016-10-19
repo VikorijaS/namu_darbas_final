f6cea301dee53ba0d0aba4c1fbe2aa7e586e0c6b
ViktorijaS <v.cerniavskyte@gmail.com>
2016-10-19 19:57:19 +0300

    override metodas, keli konstruktoriai su vienodais pavadinimais naudojami skirtingai

--- 5 paskaita/src/vcs/Uzduotis/User.java
+++ 5 paskaita/src/vcs/Uzduotis/User.java
@@ -7,6 +7,19 @@ public class User {
     private String vardas;
     private String pavarde;
     private final String klasesVardas;
+    private int amzius = 77;
+
+    public int getAmzius() {
+        return amzius;
+    }
+    public int getAmzius (int plius){
+        return getAmzius() + plius;
+    }
+    public int getAmzius (String plius){
+        return getAmzius() + new Integer(plius);
+    }
+
+
 
     /**
      * User klase konstriuktoriuje
@@ -15,17 +28,22 @@ public class User {
      */
 
     public User (String vardas, String pavarde) {
-        this.vardas = vardas;
+        this(vardas);
         this.pavarde = pavarde;
-        this.klasesVardas = this.getClass().getName();
-
-
 
     }
+
+    public User (String vardas) {
+        super();
+        this.vardas = vardas;
+        this.klasesVardas = this.getClass().getName();
+    }
+
     @Override
 
     public String toString(){
-        String result = this.klasesVardas + ":" +
+        String result = "super: " + super.toString() +
+               " " + this.klasesVardas + ":" +
                 " vardas - " + this.vardas +
                 " pavarde - " + this.pavarde;
         return result;
